ea1e4d51af8d8bb6fdfb7cf20182249956b3f8ca
ryuji-jp <61535783+ryuji-jp@users.noreply.github.com>
2021-07-05 23:45:11 +0900

    Add files via upload

--- cmd.py
+++ cmd.py
@@ -3,5 +3,31 @@
 def comand(list):
     
     if "vlan" in list:
+        list2 = list.split("vlan")[0]
         print(list)
+        print(list2)
+        return list2
 
+    if "flowctl" in list:
+        list2 = list.split("flowctl")[0]
+        print(list)
+        print(list2)
+        return list2
+
+    if "use" in list:
+        list2 = list.split("use")[0]
+        print(list)
+        print(list2)
+        return list2
+
+    if "address" in list:
+        list2 = list.split("address")[0]
+        print(list)
+        print(list2)
+        return list2
+
+    if "nat" in list:
+        list2 = list.split("nat")[0]
+        print(list)
+        print(list2)
+        return list2
